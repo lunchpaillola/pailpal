226a278a8bf734dd0c0a25268701db8021ce54cb
Lola-Ojabowale <lola.ojabowale@gmail.com>
2023-03-31 00:35:28 -0400

    simplifying the sign in page

--- pages/sign-in/[[...index]].tsx
+++ pages/sign-in/[[...index]].tsx
@@ -8,24 +8,22 @@ const SignInPage = () => (
         visibility: hidden;
       }`}
     </style>
+    
   <div style={{ 
     display: 'flex', 
     flexDirection: 'column', 
     justifyContent: 'center', 
     alignItems: 'center', 
-    height: '100vh',
+    minHeight: '100vh',
     backgroundColor: '#1f2de6',
     fontFamily: 'Inter, sans-serif',
     color: 'white',
     textAlign: 'center',
     padding: '0 20px'
   }}>
-    <h1 style={{ fontSize: '44px', fontWeight: 900 }}>Welcome to Pail Pal</h1>
-    <h2 style={{ fontSize: '24px', fontWeight: 400,}}>
-      Pail Pal is the private AI assistant for the team at  <a href="https://www.lunchpaillabs.com/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'underline' }}>Lunch Pail Labs</a>.
-    </h2>
+    <h1 style={{ fontSize: '44px', fontWeight: 900 }}>PAIL PAL</h1>
     <h2 style={{ fontSize: '24px', fontWeight: 400, marginBottom: '40px' }}>
-      If you are a Lunch Pail team member, please sign in below.
+      The private AI assistant for the <a href="https://www.lunchpaillabs.com/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'underline' }}>Lunch Pail Labs</a> team.
     </h2>
     <SignIn path="/sign-in" routing="path" afterSignInUrl="/" />
   </div>
