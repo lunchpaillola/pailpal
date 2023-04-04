eb4cc6070ca9714eaf95a4bbd5570b27c01bbad7
Lola-Ojabowale <lola.ojabowale@gmail.com>
2023-04-04 14:33:27 -0400

    adding a chains folder for more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ import { useRouter } from "next/router";
 //   "/foo"           for pages/foo/index.js
 //   "/foo/bar"       for pages/foo/bar.js
 //   "/foo/[...bar]"  for pages/foo/[...bar].js
-const publicPages = ["/sign-in/[[...index]]", "/api/lang"];
+const publicPages = ["/sign-in/[[...index]]"];
 
 const inter = Inter({ subsets: ['latin'] });
 
