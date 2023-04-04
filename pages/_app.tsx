80bcfaa5fbd57f8e6725a075daef4f083159f44d
Lola-Ojabowale <lola.ojabowale@gmail.com>
2023-04-04 16:06:21 -0400

    updating public pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ import { useRouter } from "next/router";
 //   "/foo"           for pages/foo/index.js
 //   "/foo/bar"       for pages/foo/bar.js
 //   "/foo/[...bar]"  for pages/foo/[...bar].js
-const publicPages = ["/sign-in/[[...index]]", "/api/chains/ProductSearch"];
+const publicPages = ["/sign-in/[[...index]]", "/api/chains/productsearch"];
 
 const inter = Inter({ subsets: ['latin'] });
 
